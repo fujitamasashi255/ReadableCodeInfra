81d548e2a6812a7698c0ce04cc75abbcc44f67d9
Masashi Fujita <masashifujita536@gmail.com>
2023-06-29 21:25:46 +0900

    fix typo and execute 'terraform apply'

--- envs/prod/network/main/nat_gateway.tf
+++ envs/prod/network/main/nat_gateway.tf
@@ -1,5 +1,5 @@
 resource "aws_nat_gateway" "this" {
-  for_each = var.enable_nat_gateway ? local.nat_gateway.azs : {}
+  for_each = var.enable_nat_gateway ? local.nat_gateway_azs : {}
 
   allocation_id = aws_eip.nat_gateway[each.key].id
   subnet_id     = aws_subnet.public[each.key].id
