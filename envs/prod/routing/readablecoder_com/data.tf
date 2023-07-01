b690f50be146c3fda758309ea685df652fc49142
Masashi Fujita <masashifujita536@gmail.com>
2023-07-02 01:24:03 +0900

    set up alb logs

--- envs/prod/routing/readablecoder_com/data.tf
+++ envs/prod/routing/readablecoder_com/data.tf
@@ -7,3 +7,13 @@ data "terraform_remote_state" "network_main" {
     region = "ap-northeast-1"
   }
 }
+
+data "terraform_remote_state" "log_alb" {
+  backend = "s3"
+
+  config = {
+    bucket = local.backend_bucket_name
+    key    = "${local.env_name}/log/alb.tfstate"
+    region = "ap-northeast-1"
+  }
+}
