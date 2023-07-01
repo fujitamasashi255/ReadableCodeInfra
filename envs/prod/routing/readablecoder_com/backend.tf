14e6911adc1851570819a5160f30370b0f849bc6
Masashi Fujita <masashifujita536@gmail.com>
2023-07-01 17:15:59 +0900

    setup route53 and acm then execute terraform apply

--- envs/prod/routing/readablecoder_com/backend.tf
+++ envs/prod/routing/readablecoder_com/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "readable-coder-tfstate"
-    key    = "prod/routing/readablecoder.tfstate"
+    key    = "prod/routing/readablecoder_com.tfstate"
     region = "ap-northeast-1"
   }
 }
