1a61912fdc41887dd64c41683566a0686d1dfbe4
Masashi Fujita <masashifujita536@gmail.com>
2023-07-01 18:50:34 +0900

    setup alb

--- envs/prod/shared_locals.tf
+++ envs/prod/shared_locals.tf
@@ -1,5 +1,6 @@
 locals {
-  name_prefix = "${local.app_name}-${local.env_name}"
-  app_name    = "readable-coder"
-  env_name    = "prod"
+  name_prefix         = "${local.app_name}-${local.env_name}"
+  app_name            = "readable-coder"
+  env_name            = "prod"
+  backend_bucket_name = "readable-coder-tfstate"
 }
