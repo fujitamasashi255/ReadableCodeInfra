75fa161e1e49b8c67c1bb113510b250566f3ba39
Masashi Fujita <masashifujita536@gmail.com>
2023-07-02 17:11:29 +0900

    create task iam role for Ecs Exec
    fix typo in ssm iam role

--- envs/prod/app/iam.tf
+++ envs/prod/app/iam.tf
@@ -43,7 +43,7 @@ resource "aws_iam_policy" "ssm" {
             "ssm:GetParameters",
             "ssm:GetParameter"
           ],
-          "Resource" : "arn:aws:${data.aws_region.current.id}:${data.aws_caller_identity.self.account_id}:parameter/${local.app_name}/${local.env_name}/*"
+          "Resource" : "arn:aws:ssm:${data.aws_region.current.id}:${data.aws_caller_identity.self.account_id}:parameter/${local.app_name}/${local.env_name}/*"
         }
       ]
     }
@@ -58,4 +58,50 @@ resource "aws_iam_policy" "ssm" {
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_ssm" {
   role       = aws_iam_role.ecs_task_execution.name
   policy_arn = aws_iam_policy.ssm.arn
+}
+
+resource "aws_iam_role" "ecs_task" {
+  name = "${local.name_prefix}-ecs-task"
+
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Principal" : {
+            "Service" : "ecs-tasks.amazonaws.com"
+          },
+          "Action" : "sts:AssumeRole"
+        }
+      ]
+    }
+  )
+
+  tags = {
+    Name = "${local.name_prefix}-ecs-tasks"
+  }
+}
+
+resource "aws_iam_role_policy" "ecs_task_ssm" {
+  name = "ssm"
+  role = aws_iam_role.ecs_task.id
+
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "ssmmessages:CreateControlChannel",
+            "ssmmessages:CreateDataChannel",
+            "ssmmessages:OpenControlChannel",
+            "ssmmessages:OpenDataChannel",
+          ],
+          "Resource" : "*"
+        }
+      ]
+    }
+  )
 }
