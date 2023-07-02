239109b914fa296054e1ed8b065b43227f70184b
Masashi Fujita <masashifujita536@gmail.com>
2023-07-02 16:57:59 +0900

    add iam policy to reference parameter store to ecs task execution

--- envs/prod/app/iam.tf
+++ envs/prod/app/iam.tf
@@ -29,3 +29,33 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
   role       = aws_iam_role.ecs_task_execution.name
   policy_arn = data.aws_iam_policy.ecs_task_execution.arn
 }
+
+resource "aws_iam_policy" "ssm" {
+  name = "${local.name_prefix}-ssm"
+
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "ssm:GetParameters",
+            "ssm:GetParameter"
+          ],
+          "Resource" : "arn:aws:${data.aws_region.current.id}:${data.aws_caller_identity.self.account_id}:parameter/${local.app_name}/${local.env_name}/*"
+        }
+      ]
+    }
+  )
+
+  tags = {
+    Name = "${
+    local.name_prefix}-ssm"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_ssm" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = aws_iam_policy.ssm.arn
+}
