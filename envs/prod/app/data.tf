72b319ded9a5d52a5790aef720229c1eea739471
Masashi Fujita <masashifujita536@gmail.com>
2023-07-04 02:32:22 +0900

    setup alb target group

--- envs/prod/app/data.tf
+++ envs/prod/app/data.tf
@@ -1,3 +1,13 @@
 data "aws_caller_identity" "self" {}
 
 data "aws_region" "current" {}
+
+data "terraform_remote_state" "network_main" {
+  backend = "s3"
+
+  config = {
+    bucket = local.backend_bucket_name
+    key    = "prod/network/main.tfstate"
+    region = "ap-northeast-1"
+  }
+}
