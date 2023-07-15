27fddeb9a609819b4181230b8f3cd1b0eb0862dd
Masashi Fujita <masashifujita536@gmail.com>
2023-07-15 19:18:24 +0900

    create route53 cname record for rds

---
+++ envs/prod/routing/readablecoder_internal/data.tf
@@ -0,0 +1,19 @@
+data "terraform_remote_state" "network_main" {
+  backend = "s3"
+
+  config = {
+    bucket = "readable-coder-tfstate"
+    key    = "prod/network/main.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+data "terraform_remote_state" "db_readablecoder" {
+  backend = "s3"
+
+  config = {
+    bucket = "readable-coder-tfstate"
+    key    = "prod/db.tfstate"
+    region = "ap-northeast-1"
+  }
+}
