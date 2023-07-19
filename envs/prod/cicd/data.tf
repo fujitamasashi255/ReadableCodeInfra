9410a037730601abd6173df74ee6f7eed8593a08
Masashi Fujita <masashifujita536@gmail.com>
2023-07-20 03:16:53 +0900

    add permission of executing ecspresso register and run to iam deploy role

--- envs/prod/cicd/data.tf
+++ envs/prod/cicd/data.tf
@@ -1,3 +1,11 @@
+data "aws_caller_identity" "self" {}
+
+data "aws_region" "current" {}
+
+data "aws_ecs_cluster" "this" {
+  cluster_name = local.name_prefix
+}
+
 data "aws_ecs_service" "this" {
   cluster_arn  = local.name_prefix
   service_name = local.name_prefix
